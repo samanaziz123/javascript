8ebe66975628e308119dcda4e9fc2ec90a6c44bf
samanaziz123 <samanaziz744@gmail.com>
2024-05-02 15:30:26 +0500

    message

---
+++ chapter 1/app.js
@@ -0,0 +1,5 @@
+alert("JAVASCRIPT ALERT \n Error! please enter a valid password");
+alert("JAVASCRIPT ALERT \n Welcome to JS land \n happy coding!");
+alert("JAVASCRIPT ALERT \n Welcome to JS land");
+alert("JAVASCRIPT ALERT \n Happy coding! \n prevent this page from creating the additional dialogue");
+alert("JAVASCRIPT ALERT \n hello..... i can run JS from my web browser console");
